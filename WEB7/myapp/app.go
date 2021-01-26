38434b38e9e85b9dacbb817d7b0a4f3b5c7bde4c
DESKTOP-2JI1BTP\solomon02 <gold0073@naver.com>
2021-01-26 16:33:50 +0900

    WEB7 Start Commit

---
+++ WEB7/myapp/app.go
@@ -0,0 +1,91 @@
+package myapp
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+var userMap map[int]*User
+var lastID int
+
+//User is...
+type User struct {
+	ID        int       `json:"id"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+func usersHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Get User in for by /users/{id}")
+}
+
+func getUserInforHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id, err := strconv.Atoi(vars["id"])
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	user, ok := userMap[id]
+	if !ok {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, "No User ID:", id)
+		return
+	}
+
+	w.Header().Add("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
+}
+
+func createUserHandler(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, err)
+		return
+	}
+
+	//Created User
+	lastID++
+	user.ID = lastID
+	user.CreatedAt = time.Now()
+	userMap[user.ID] = user
+
+	w.Header().Add("Content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	data, _ := json.Marshal(user)
+	fmt.Fprint(w, string(data))
+
+}
+
+//NewHandler is
+func NewHandler() http.Handler {
+	userMap = make(map[int]*User)
+	lastID = 0
+
+	mux := mux.NewRouter()
+	//mux := http.NewServeMux()
+	mux.HandleFunc("/", indexHandler)
+	mux.HandleFunc("/users", usersHandler).Methods("GET")
+	mux.HandleFunc("/users", createUserHandler).Methods("POST")
+	mux.HandleFunc("/users/{id:[0-9]+}", getUserInforHandler)
+	return mux
+}
